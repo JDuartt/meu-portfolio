2d9200b5e682cbbcafb4be2662b2a8f2f4f56ff1
JDuartt <142469865+JDuartt@users.noreply.github.com>
2023-08-18 02:09:43 -0300

    Add files via upload

---
+++ html e bootsrap/git.cs
@@ -0,0 +1,7 @@
+echo "# meu-portfolio" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/JDuartt/meu-portfolio.git
+git push -u origin main
